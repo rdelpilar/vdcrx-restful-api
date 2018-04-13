3dae262f4f74f05a332c718241ab7a76d1863553
Ranel del Pilar <ranel.del.pilar@gmail.com>
2018-04-12 20:51:29 -0700

    VdcrxRestApiApplication

---
+++ src/main/java/com/vdcrx/rest/VdcrxRestApiApplication.java
@@ -0,0 +1,18 @@
+package com.vdcrx.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application entry point
+ *
+ * @author Ranel del Pilar
+ */
+
+@SpringBootApplication
+public class VdcrxRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VdcrxRestApiApplication.class, args);
+    }
+}
