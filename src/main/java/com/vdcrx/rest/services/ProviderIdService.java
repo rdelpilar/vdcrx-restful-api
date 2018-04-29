a77e28aa5505b741b31bf1b3d3a3fc3563b731de
Ranel del Pilar <ranel.del.pilar@gmail.com>
2018-04-29 14:10:26 -0700

    Provider id service

---
+++ src/main/java/com/vdcrx/rest/services/ProviderIdService.java
@@ -0,0 +1,33 @@
+package com.vdcrx.rest.services;
+
+import com.vdcrx.rest.api.v1.model.dto.ProviderIdDto;
+import org.springframework.dao.DataAccessException;
+
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Provider identifier service interface
+ *
+ * @author Ranel del Pilar
+ */
+
+public interface ProviderIdService {
+
+    /* Create */
+    ProviderIdDto createProviderIdentifier(final UUID id, final ProviderIdDto resource) throws DataAccessException;
+
+    /* Update */
+    ProviderIdDto updateProviderIdentifier(final UUID id, final UUID provider_id, ProviderIdDto resource) throws DataAccessException;
+
+    /* Delete */
+    void deleteProviderIdentifier(final UUID id, final UUID provider_id) throws DataAccessException;
+
+    /* Retrieve */
+    ProviderIdDto findProviderIdById(final UUID id);
+    ProviderIdDto findProviderIdByPersonId(final UUID id);
+    ProviderIdDto findProviderIdByUsername(final String username);
+    ProviderIdDto findProviderIdByEmail(final String email);
+    ProviderIdDto findProviderIdByPhone(final String phone);
+    Set<ProviderIdDto> findProviderIdsByUsernameCol(final Set<String> usernameSet);
+}
